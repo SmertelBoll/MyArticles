79ef01618cecd6e714d562020727491160fcb3e5
Lyubomyr <sholop2113@gmail.com>
2023-04-09 16:43:31 +0300

    start server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// на якому хості запускаємо, функція що робити якщо помилка
+app.listen(4444, (err) => {
+  if (err) {
+    console.log("server error");
+    console.log(err);
+  }
+
+  console.log("server ok");
+});
