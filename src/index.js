de34549c2344dab095cffad1f82020dc1ec4bcc1
Lyubomyr <sholop2113@gmail.com>
2023-04-29 23:36:44 +0300

    added the basename

--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-// import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
-// import { theme } from "./theme/theme";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <ThemeProvider theme={theme}>
-  <BrowserRouter>
+  <BrowserRouter basename="https://fancy-kheer-2d2031.netlify.app/">
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
-  // </ThemeProvider>
 );
