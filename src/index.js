b774722015d8952f314ebbf49218af1c5c830570
Lyubomyr <sholop2113@gmail.com>
2023-04-29 23:41:48 +0300

    fix routing

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="https://fancy-kheer-2d2031.netlify.app/">
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
